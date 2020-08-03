5acd8d0a6b81b5ea26c0f192d4a05099d6499929
LucasFDutra <lucasfelipedutra@gmail.com>
2020-08-03 19:31:40 -0300

    landing

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Landing from './pages/Landing/index';
+import './assets/syles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
